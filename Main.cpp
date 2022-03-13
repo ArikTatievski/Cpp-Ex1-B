6574f498f67fa3a2fe16684db94d52ec402623e7
ArikTatievski <ariktatievski@gmail.com>
2022-03-13 11:28:25 -0700

    Final commit

--- Main.cpp
+++ Main.cpp
@@ -7,11 +7,13 @@ int main() {
     int x,y;
     char a,b;
     std::cout << "Please enter 2 numbers (matrix size) and 2 characters (carpet symbols)" << std::endl;
+    //Accepting values for our matrix
     std::cin >> x;
     std::cin >> y;
     std::cin >> a;
     std::cin >> b;
     string ans = ariel::mat(x,y,a,b);
+    //Printing the reasult
     std::cout << ans << std::endl;
     return 0;
 }
--- mat.cpp
+++ mat.cpp
@@ -8,14 +8,17 @@
 
 using namespace std;
 string ariel::mat(int y,int x,char a,char b){
+    //MAX&MIN VALUES are valid keys
     int const MAX_VALUE = 126;
     int const MIN_VALUE = 33;
+    //Check for non even & non negative numbers
     if (x <= 0 || y <= 0){
         throw  std::invalid_argument("Mat size has to be a positive number above 0");
     }
     if (x%2 == 0 || y%2 == 0){
         throw std::invalid_argument("Mat size has to be odd");
     }
+    //Valid keys check
     if (a < MIN_VALUE || a > MAX_VALUE){
         throw std::invalid_argument("First char parameter is invalid");
     }
@@ -29,7 +32,9 @@ string ariel::mat(int y,int x,char a,char b){
     int minskip = std::min(x,y);
     minskip = minskip / 2;
     char currchar = a;
+    //First loop to check "Which matrix am I filling now?
     for (int skip = 0; skip <= minskip; ++skip) {
+        //Second 2 loops are to fill the first & last line and filling first & last column
         for (int i = 0; i < y-(2*skip); ++i) {
             curr_mat[skip][i+skip] = currchar;
             curr_mat[x-skip-1][i+skip] = currchar;
@@ -38,6 +43,7 @@ string ariel::mat(int y,int x,char a,char b){
             curr_mat[i+skip][skip] = currchar;
             curr_mat[i+skip][y-skip-1] = currchar;
         }
+        //Switching keys between iterations
         if (currchar == a){
             currchar = b;
         }
@@ -47,6 +53,7 @@ string ariel::mat(int y,int x,char a,char b){
     }
 
     std::string ans;
+    //Throwing the values into a string
     for (int i = 0; i < x; ++i) {
         for (int j = 0; j < y; ++j) {
             ans += curr_mat[i][j];
